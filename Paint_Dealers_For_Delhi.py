5067fe9db43816469bc590e114bf1b29730c8de9
Priya Chandak <priya.chandak@vishleshak.io>
2021-12-28 14:35:23 +0530

    updated files

--- Paint_Dealers_For_Delhi.py
+++ Paint_Dealers_For_Delhi.py
@@ -14,20 +14,26 @@ import requests
 from selenium import webdriver
 import time
 import re
+import urllib.parse
+import pandas as pd
+import json
+import math
+from geopy import geocoders
+from geopy.geocoders import Nominatim
 
 
 # In[ ]:
 
 
 options = webdriver.ChromeOptions() 
-driver = webdriver.Chrome(executable_path=r'{"path_to_your_chromedrive"}/chromedriver.exe')
+driver = webdriver.Chrome(executable_path=r'<Path to your chromedriver>/chromedriver.exe')
 
 name1=[]
 address1=[]
 contact1=[]
 dealers1=[]
 
-paint_dealers=['asian-paints','british-paints','dulux','kansai-nerolac','nippon-paint','shalimar','mrf','indigo-paints']
+paint_dealers=['asian-paints','british-paints','dulux','kansai-nerolac','nippon-paint','jotun','jsw-paints','shalimar','mrf','indigo-paints']
 
 for i in range(0,len(paint_dealers)):
     url = "https://www.dealerservicecenter.in/list/paint/{}/delhi/delhi".format(paint_dealers[i])
@@ -64,8 +70,8 @@ for i in range(0,len(paint_dealers)):
 # In[ ]:
 
 
-df=pd.DataFrame(list(zip(name1,address1,contact1,dealers1)),columns=['Name','Address','Contact','Dealer'])
-df.head()
+final_data=pd.DataFrame(list(zip(name1,address1,contact1,dealers1)),columns=['Name','Address','Contact','Dealer'])
+len(final_data)
 
 
 # #### By running this script you will get the data for Delhi Paint Dealers for the brands mentioned above 
@@ -73,58 +79,6 @@ df.head()
 # In[ ]:
 
 
-name2=[]
-address2=[]
-contact2=[]
-dealers2=[]
-
-paint_dealers=['asian-paints','british-paints','dulux','kansai-nerolac','nippon-paint','shalimar','mrf','indigo-paints']
-
-for i in range(0,len(paint_dealers)):
-    url = "https://www.dealerservicecenter.in/list/paint/{}/delhi/new-delhi".format(paint_dealers[i])
-    
-    driver.get(url)
-    r = requests.get(url)  
-    soup = BeautifulSoup(r.content, 'lxml')
-    table = soup.findAll('div',{'class':'panel panel-default testimonial'})
-    print(url)
-    
-        
-    for j in range(5,len(table)):
-        try:
-            name = driver.find_element_by_xpath("//*[@id='content']/div/div/div[1]/div/div/div/div[{}]/div/h3/a".format(j)).text
-            address = driver.find_element_by_xpath("//*[@id='content']/div/div/div[1]/div/div/div/div[{}]/div/ul/li[1]".format(j)).text
-            contact = driver.find_element_by_xpath("//*[@id='content']/div/div/div[1]/div/div/div/div[{}]/div/ul/li[3]".format(j)).text
-            name2.append(name)
-            address2.append(address)
-            contact2.append(contact)
-            dealers2.append(paint_dealers[i])
-        except Exception as ex:
-            name = driver.find_element_by_xpath("//*[@id='content']/div/div/div[1]/div/div/div/div[{}]/div/h3/a".format(j+1)).text
-            address = driver.find_element_by_xpath("//*[@id='content']/div/div/div[1]/div/div/div/div[{}]/div/ul/li[1]".format(j+1)).text
-            contact = driver.find_element_by_xpath("//*[@id='content']/div/div/div[1]/div/div/div/div[{}]/div/ul/li[3]".format(j+1)).text
-            name2.append(name)
-            address2.append(address)
-            contact2.append(contact)
-            dealers2.append(paint_dealers[i])
-            print(ex)
-        
-        
-
-
-# In[ ]:
-
-
-df1=pd.DataFrame(list(zip(name2,address2,contact2,dealers2)),columns=['Name','Address','Contact','Dealer'])
-df1.head()
-len(df1)
-
-
-# In[ ]:
-
-
-data=[df,df1]
-final_data = pd.concat(data)
 final_data.drop_duplicates(inplace=True)
 final_data=pd.DataFrame(final_data)
 final_data.reset_index(inplace=True)
@@ -178,5 +132,30 @@ final_data
 # In[ ]:
 
 
-final_data.to_excel('path/file_name.xlsx',index=False)
+data1=pd.DataFrame(final_data)
+
+pincode=data1['Pincode']
+
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent='my_appli')
+
+
+for i in pincode:
+    try:
+        if i is not None :
+            location = geolocator.geocode(i)
+            #data1['lat'].append(location.latitude)
+            
+            #print(location.address)
+            print((location.latitude,location.longitude))
+    except:
+        print('- -')
+data1.insert(5, 'latitude', location.latitude, allow_duplicates=False)
+data1.insert(6, 'longitude', location.longitude, allow_duplicates=False)
+
+
+# In[ ]:
+
+
+data1.to_excel("<path to save your file> + <filename>.xlsx",index=False)
 
